f37fe9ad35864e184cc48ca54bf2f8ae1c0791b8
Tu Nguyen <naterpaul@gmail.com>
2019-05-28 16:24:24 -0500

    allow streaming in chunk

--- routes/video.js
+++ routes/video.js
@@ -23,19 +23,36 @@ router.get('/:path*', function(req, res, next) {
     var stream = fs.createReadStream(path);
 	
 
-	//console.log(res);		
-    stream.on('open', () => {
-        console.log("Start Steaming");
-        stream.pipe(res);
-    });
-
-    stream.on('error', (err) => {
-        console.log(err);
-    });
-
-    stream.on('close', () => {
-        console.log("FILE CLOSED");
-    });
+	const stat = fs.statSync(path)
+	const fileSize = stat.size
+	const range = req.headers.range
+	
+	if (range) {
+	  	const parts = range.replace(/bytes=/, "").split("-")
+	  	const start = parseInt(parts[0], 10)
+	  	const end = parts[1]
+	  	  ? parseInt(parts[1], 10)
+	  	  : fileSize-1
+	
+	  	const chunksize = (end-start)+1
+	  	const file = fs.createReadStream(path, {start, end})
+	  	const head = {
+	  	  'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+	  	  'Accept-Ranges': 'bytes',
+	  	  'Content-Length': chunksize,
+	  	  'Content-Type': 'video/mp4',
+	  }
+	
+	  res.writeHead(206, head)
+	  file.pipe(res)
+	} else {
+	  const head = {
+	    'Content-Length': fileSize,
+	    'Content-Type': 'video/mp4',
+	  }
+	  res.writeHead(200, head)
+	  fs.createReadStream(path).pipe(res)
+	}
 
 });
 module.exports = router;
